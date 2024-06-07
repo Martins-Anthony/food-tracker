85349b4d1d1f30a5a958f482e1b39dcdc487092b
Martins <thony.martins.67@gmail.com>
2024-06-07 12:54:38 +0200

    unit test : component Cards

---
+++ frontend/src/Components/Cards/Cards.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Cards from './index'
+
+describe('Cards Component', () => {
+  test('renders card with title and text', () => {
+    const title = 'Test Title'
+    const text = 'Test Text'
+
+    render(<Cards title={title} text={text} />)
+
+    expect(screen.getByText(title)).toBeInTheDocument()
+
+    expect(screen.getByText(text)).toBeInTheDocument()
+
+    expect(screen.getByAltText('...')).toHaveAttribute(
+      'src',
+      'https://picsum.photos/300/150?random=2'
+    )
+  })
+})
