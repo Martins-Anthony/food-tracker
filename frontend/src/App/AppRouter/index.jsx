6246f56ee8bc7fce042273338a00ba251d59d3f2
Martins <thony.martins.67@gmail.com>
2024-06-12 15:47:46 +0200

    moving the Sidebar component to the AppRouter file and refactoring the Sidebar component

--- frontend/src/App/AppRouter/index.jsx
+++ frontend/src/App/AppRouter/index.jsx
@@ -4,6 +4,7 @@ import Home from '../../Pages/Home'
 import '../../style/index.scss'
 import Header from '../../Containers/Layouts/Header'
 import Footer from '../../Containers/Layouts/Footer'
+import Sidebar from '../../Containers/Layouts/Sidebar'
 import Settings from '../../Pages/Settings'
 import Error from '../../Pages/Error'
 import AddProduct from '../../Pages/AddProduct'
@@ -22,21 +23,24 @@ function AppRouter() {
     <HashRouter>
       <div className="d-flex flex-column min-vh-100">
         <Header />
-        <main className="text-center d-flex flex-column flex-grow-1">
-          <Routes>
-            <Route
-              path="/"
-              element={isAuthenticated ? <ProtectedRoute element={User} /> : <Home />}
-            />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/enter/:email/:magicLink" element={<Enter />} />
-            <Route path="/user" element={<ProtectedRoute element={User} />} />
-            <Route path="/settings" element={<ProtectedRoute element={Settings} />} />
-            <Route path="/addProduct" element={<ProtectedRoute element={AddProduct} />} />
-            <Route path="*" element={<Error />} />
-          </Routes>
-        </main>
+        <div className="d-flex flex-grow-1">
+          {isAuthenticated ? <Sidebar /> : null}
+          <main className="text-center d-flex flex-column flex-grow-1">
+            <Routes>
+              <Route
+                path="/"
+                element={isAuthenticated ? <ProtectedRoute element={User} /> : <Home />}
+              />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/enter/:email/:magicLink" element={<Enter />} />
+              <Route path="/user" element={<ProtectedRoute element={User} />} />
+              <Route path="/settings" element={<ProtectedRoute element={Settings} />} />
+              <Route path="/addProduct" element={<ProtectedRoute element={AddProduct} />} />
+              <Route path="*" element={<Error />} />
+            </Routes>
+          </main>
+        </div>
         <Footer />
       </div>
     </HashRouter>
--- frontend/src/Containers/Layouts/Sidebar/index.jsx
+++ frontend/src/Containers/Layouts/Sidebar/index.jsx
@@ -1,11 +1,36 @@
+import React, { useState } from 'react'
+import { iconsLibrary } from '../../../Components/Icons/library'
 function Sidebar() {
+  const [isSidebarVisible, setSidebarVisible] = useState(false)
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!isSidebarVisible)
+  }
+
+  const hideSidebar = () => {
+    setSidebarVisible(false)
+  }
+
+  const chevronIconName = isSidebarVisible ? 'chevron-left' : 'chevron-right'
+  const chevronIcon = iconsLibrary.sidebar.find((icon) => icon.name === chevronIconName).icon
+
   return (
     <>
-      <ul className="nav nav-tabs flex-column bg-light navbar-light fixed-right h-100 pt-5">
-        <li className="nav-item">
-          <button className="btn btn-primary">zone de stockage</button>
-        </li>
-      </ul>
+      <div className={`col-2 p-0 sidebar text-center ${isSidebarVisible ? 'show' : ''}`}>
+        <ul className="nav nav-tabs flex-column bg-light navbar-light fixed-right h-100 pt-5">
+          <li className="nav-item">
+            <button className="btn btn-primary">zone de stockage</button>
+          </li>
+        </ul>
+      </div>
+      <div className="d-md-none p-2 mt-5">
+        <button className="btn btn-primary" onClick={toggleSidebar}>
+          {chevronIcon}
+        </button>
+      </div>
+      <div
+        className={`sidebar-overlay ${isSidebarVisible ? 'show' : ''}`}
+        onClick={hideSidebar}></div>
     </>
   )
 }
