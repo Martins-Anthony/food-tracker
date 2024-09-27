e94b57ec47af47ade3dbd76cfa2d8d8bd69d992b
Martins <thony.martins.67@gmail.com>
2024-09-27 21:42:04 +0200

    Refactor(basePath.js) : Modified for environment variables

--- frontend/src/utils/api/basePath.js
+++ frontend/src/utils/api/basePath.js
@@ -1,4 +1,5 @@
-const address = 'http://localhost'
-const basePath = '/api/'
-const version = 'v1'
+const address = process.env.REACT_APP_ADDRESS
+const basePath = process.env.REACT_APP_BASE_PATH
+const version = process.env.REACT_APP_VERSION_APP
+
 export const urlApi = address + basePath + version
